af13dc61c27b378941bfb9a480add49a64422e45
Mdubane Digits <65094648+Digital-101@users.noreply.github.com>
2024-10-28 16:08:47 +0200

    Added Faculties and Institutions
    Changes Made
    
    Co-Authored-By: Mzilakatha11 <177626937+Mzilakatha11@users.noreply.github.com>

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/faculties')
+def faculties():
+    return render_template('faculties.html')
+
+@app.route('/institutions')
+def institutions():
+    return render_template('institutions.html')
+
 @app.route('/accounting')
 def accounting():
     return render_template('accounting.html')
